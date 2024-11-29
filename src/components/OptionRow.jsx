172fc65c0289c8b03f8edbf54f25a699a9cc8fba
cmkwong <cheung.mankwong@yahoo.com.hk>
2024-11-29 10:40:31 +0800

    OptionRow can click and update the options

--- src/components/OptionRow.jsx
+++ src/components/OptionRow.jsx
@@ -1,23 +1,34 @@
+import { useRef } from 'react';
 import styles from './OptionRow.module.css';
 
 const OptionRow = (props) => {
   let { id, label, checked, updateOptionData } = props;
 
+  const checkboxRef = useRef(null);
+
   const handleChange = (event) => {
     updateOptionData(parseInt(event.target.id), event.target.checked);
   };
 
+  const handleClick = (event) => {
+    checkboxRef.current.checked = !checkboxRef.current.checked;
+    updateOptionData(parseInt(id), checkboxRef.current.checked);
+  };
+
   return (
-    <div className={styles['container']}>
+    <div id={id} className={styles['container']} onClick={handleClick}>
       <input
         className={styles['checkbox']}
+        ref={checkboxRef}
         id={id}
         name={label}
         defaultChecked={checked}
         onChange={handleChange}
         type="checkbox"
       />
-      <h4 className={styles['label']}>{label}</h4>
+      <h4 id={id} className={styles['label']}>
+        {label}
+      </h4>
     </div>
   );
 };

59fd6e5df6b95ebc7787d43b4557fc95a18bafe5
cmkwong <cheung.mankwong@yahoo.com.hk>
2024-11-29 09:52:54 +0800

    filter the tags

--- src/components/InputOption.jsx
+++ src/components/InputOption.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import styles from './InputOption.module.css';
 import { useRef } from 'react';
 import OptionRow from './OptionRow';
@@ -6,26 +6,28 @@ import TagPlate from './tagPlate';
 
 const InputOption = () => {
   let _options = [
-    { id: 1, label: 'pet brush', checked: true, show: true },
-    { id: 2, label: 'pet mats', checked: false, show: true },
-    { id: 3, label: 'clean up', checked: false, show: true },
-    { id: 4, label: 'clipper', checked: false, show: true },
-    { id: 5, label: 'shower', checked: false, show: true },
-    { id: 6, label: 'headwears', checked: true, show: true },
-    { id: 7, label: 'tops', checked: false, show: true },
-    { id: 8, label: 'Pet Bowl', checked: false, show: true },
-    { id: 9, label: 'Drinking Tools', checked: false, show: true },
-    { id: 10, label: 'Feeding Tools', checked: false, show: true },
-    { id: 11, label: 'Glasses', checked: false, show: true },
-    { id: 12, label: 'collar', checked: false, show: true },
-    { id: 13, label: 'leash', checked: false, show: true },
+    { id: 1, label: 'pet brush', checked: true },
+    { id: 2, label: 'pet mats', checked: false },
+    { id: 3, label: 'clean up', checked: false },
+    { id: 4, label: 'clipper', checked: false },
+    { id: 5, label: 'shower', checked: false },
+    { id: 6, label: 'headwears', checked: true },
+    { id: 7, label: 'tops', checked: false },
+    { id: 8, label: 'Pet Bowl', checked: false },
+    { id: 9, label: 'Drinking Tools', checked: false },
+    { id: 10, label: 'Feeding Tools', checked: false },
+    { id: 11, label: 'Glasses', checked: false },
+    { id: 12, label: 'collar', checked: false },
+    { id: 13, label: 'leash', checked: false },
   ];
 
   const [options, setOptions] = useState(_options);
+  const [filterOptions, setFitlerOptions] = useState(_options);
 
   let inputWidth = '100%';
 
   const inputReference = useRef(null);
+  const [inputValue, setInputValue] = useState('');
   const [showOption, setShowOption] = useState(false);
   const [selectionMouseIn, setSelectionMouseIn] = useState(false);
 
@@ -61,9 +63,33 @@ const InputOption = () => {
     );
   };
   // add the value into option
-  // TODO: to avoid the duplicate tag
-  const addOptionData = (value) => {
+  // TODO: to avoid the duplicate tag //
+  const handleEnterPress = (value) => {
     if (!value) return;
+    // hide the option choice
+    setShowOption(false);
+    // clear the value after enter pressed
+    inputReference.current.value = '';
+    setInputValue('');
+
+    // finding if it has the same element, then set it into checked and no need to add new
+    let duplicatedOptions = options.filter(
+      (el) => el.label.toLowerCase() === value.toLowerCase()
+    );
+    if (duplicatedOptions.length > 0) {
+      duplicatedOptions.map((el) => {
+        updateOptionData(el.id, true);
+      });
+      return;
+    }
+
+    // add data into option
+    addOptionData(value);
+  };
+
+  // add option data
+  const addOptionData = (value) => {
+    // getting the new ID
     const max_option = options.reduce((acc, curr) => {
       return acc.id < curr.id ? curr : acc;
     });
@@ -71,26 +97,22 @@ const InputOption = () => {
       { id: max_option.id + 1, label: value, checked: true },
       ...options,
     ]);
-    // hide the option choice
-    setShowOption(false);
-    // clear the value after enter pressed
-    inputReference.current.value = '';
   };
 
   // sorting the option in a list
-  const filterOptions = (value) => {
-    if (!value) return;
-    setOptions(
-      options.map((el) => {
-        if (el.label.includes(value)) {
-          el.show = true;
-        } else {
-          el.show = false;
-        }
-        return el;
-      })
-    );
-  };
+  // const filterOptions = (value) => {
+  //   if (!value) return;
+  //   setOptions(
+  //     options.map((el) => {
+  //       if (el.label.includes(value)) {
+  //         el.show = true;
+  //       } else {
+  //         el.show = false;
+  //       }
+  //       return el;
+  //     })
+  //   );
+  // };
 
   return (
     <div className={styles['inputOption']}>
@@ -99,15 +121,15 @@ const InputOption = () => {
           className={styles['inputField']}
           type="text"
           ref={inputReference}
-          onFocus={handleFocus}
+          onClick={handleFocus}
           onBlur={handleFocusOut}
           onKeyDown={(event) => {
             if (event.key === 'Enter') {
-              addOptionData(event.target.value);
+              handleEnterPress(event.target.value);
             }
           }}
           // sorting the key words
-          onChange={(event) => filterOptions(event.target.value)}
+          onChange={() => setInputValue(inputReference.current.value)}
         />
         {showOption && (
           <div
@@ -118,17 +140,16 @@ const InputOption = () => {
             onClick={handleClickSelection}
           >
             {options.map((el) => {
-              if (el.show) {
-                return (
-                  <OptionRow
-                    key={el.id}
-                    id={el.id}
-                    label={el.label}
-                    checked={el.checked}
-                    updateOptionData={updateOptionData}
-                  />
-                );
-              }
+              if (inputValue && !el.label.includes(inputValue)) return;
+              return (
+                <OptionRow
+                  key={el.id}
+                  id={el.id}
+                  label={el.label}
+                  checked={el.checked}
+                  updateOptionData={updateOptionData}
+                />
+              );
             })}
           </div>
         )}
